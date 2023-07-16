84c846ec567cfb8d6b08e61d70c87b0aabcb010c
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-15 20:46:15 -0700

    updated a tag

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,21 +1,19 @@
 import React from "react";
 
 export default function ResumeSection() {
-    const proficiencies = ["Javascript", "React", "HTML", "Node.js"];
+  const proficiencies = ["Javascript", "React", "HTML", "Node.js"];
 
-    return (
-        <div className="resume-section">
-            <h2>Resume</h2>
-            <p>Download my resume:</p>
-            <a href="" download>
-                Comming Soon!
-            </a>
-            <h3>Proficiencies:</h3>
-            <ul>
-                {proficiencies.map((proficiency, index) => (
-                    <li key={index}>{proficiency}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="resume-section">
+      <h2>Resume</h2>
+      <p>Download my resume:</p>
+      <p>Comming Soon!</p>
+      <h3>Proficiencies:</h3>
+      <ul>
+        {proficiencies.map((proficiency, index) => (
+          <li key={index}>{proficiency}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
